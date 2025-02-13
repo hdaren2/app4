58b7a55dd735958c026a29505758ff961e55a7e7
hdaren2 <163777281+hdaren2@users.noreply.github.com>
2025-02-13 10:37:41 -0600

    Added button to go to second page

--- lib/main.dart
+++ lib/main.dart
@@ -46,19 +46,29 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      const Text(
+        'You have pushed the button this many times:',
       ),
+      Text(
+        '$_counter',
+        style: Theme.of(context).textTheme.headlineMedium,
+      ),
+      const SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SecondScreen()),
+          );
+        },
+        child: const Text('Go to Second Page'),
+      ),
+    ],
+  ),
+),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
